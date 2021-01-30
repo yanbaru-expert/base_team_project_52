5d423413470583bfc4bd5c07b4fc1947d464f2be
miya <rinn.sasami@gmail.com>
2021-01-30 21:20:36 +0900

    詳細表示機能 show を実装。users コントローラーの index アクションを修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.select("name")
+    @users = User.all
   end
 
   def new
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
