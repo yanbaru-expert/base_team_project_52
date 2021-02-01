10de51acbc0ef3123812e4dd5ee432e4e3f94cd5
zenfumi <ys00878@gmail.com>
2021-02-01 17:14:24 +0900

    新規投稿機能 new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+
+  def new
+    @message = Message.new
+  end
+
+  # createメソッド=オブジェクトの生成 + データの保存
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  # paramsはHTTPリクエストパラメーター(入力データ)のハッシュが格納されている変数
+  def message_params
+    params.require(:message).permit(:title,:content)
+  end
 end
